95fec804e27cad3f1791998e7919d2182eea8b01
Dixonrc28 <andreyrc28.cr@gmail.com>
2024-10-29 15:44:01 -0600

    version1

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// ----------------------------------------------------------------
+// Función para actualizar el texto y los botones en el carrusel
+// ----------------------------------------------------------------
+const carouselItems = document.querySelectorAll('.carousel-item');
+const titleElement = document.getElementById('hero-title');
+const buttonElement1 = document.querySelector('#hero-buttons .btn.btn-primary');
+const buttonElement2 = document.querySelector('#hero-buttons .btn.btn-outline-secondary');
+
+const updateContent = (index) => {
+    const item = carouselItems[index];
+    titleElement.innerText = item.getAttribute('data-title');
+    buttonElement1.innerText = item.getAttribute('data-button1');
+    buttonElement2.innerText = item.getAttribute('data-button2');
+};
+
+// Evento para actualizar el contenido al cambiar de diapositiva
+document.getElementById('carouselExampleIndicators').addEventListener('slide.bs.carousel', (event) => {
+    const nextIndex = event.to; // índice de la siguiente diapositiva
+    updateContent(nextIndex);
+});
+
+// Inicializa el contenido en la primera diapositiva
+updateContent(0);
+
+
+// ----------------------------------------------------------------
+// boton de busqueda en el NAV
+// ----------------------------------------------------------------
+document.getElementById('searchButton').addEventListener('click', function() {
+    const searchForm = document.getElementById('searchForm');
+    searchForm.classList.toggle('d-none');
+});
+
+
